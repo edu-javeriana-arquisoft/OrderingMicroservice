c50b9bfa1dde4e3bcf05f2591d3f62ab3beab13f
Juan Paez <jd.paez@javeriana.edu.co>
2023-11-07 22:56:13 -0800

    add product entity

--- src/main/java/com/example/orderingmicroservice/model/Product.java
+++ src/main/java/com/example/orderingmicroservice/model/Product.java
@@ -1,4 +1,22 @@
 package com.example.orderingmicroservice.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+
 }

ed239ca07c93da316d37b4b944302f26421e87e8
Juan Paez <jd.paez@javeriana.edu.co>
2023-11-19 07:10:02 -0800

    add docker files and eureka config with tomcat

--- src/main/java/com/example/orderingmicroservice/OrderingMicroserviceApplication.java
+++ src/main/java/com/example/orderingmicroservice/OrderingMicroserviceApplication.java
@@ -2,14 +2,20 @@ package com.example.orderingmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class OrderingMicroserviceApplication {
+public class OrderingMicroserviceApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderingMicroserviceApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OrderingMicroserviceApplication.class);
+	}
 }
